506e703a21669e35457b2b7bff328bc4ae2053e2
KSL-Pimentel <bruno.pimentel@sptech.school>
2024-05-29 12:08:16 -0300

    lançando mensagem de erro

--- src/test/java/sptech/school/exemploactions/service/CalculadoraServiceTest.java
+++ src/test/java/sptech/school/exemploactions/service/CalculadoraServiceTest.java
@@ -2,8 +2,10 @@ package sptech.school.exemploactions.service;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.web.server.ResponseStatusException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class CalculadoraServiceTest {
 
@@ -18,5 +20,21 @@ public class CalculadoraServiceTest {
         double resultado = calculadoraService.dividir(a, b);
 
         assertEquals(esperado, resultado);
+    }
+
+    @Test
+    @DisplayName("Quando adicionado com 10 e 0, então deve lançar uma exceção")
+    public void testDividirPorZero() {
+        CalculadoraService calculadoraService = new CalculadoraService();
+        double a = 10;
+        double b = 0;
+        var expectedMessage = "400 BAD_REQUEST \"Divisão por zero não permitida\"";
+
+        ResponseStatusException exception = assertThrows(
+                ResponseStatusException.class, () -> {
+                    calculadoraService.dividir(a, b);
+                });
+
+        assertEquals(expectedMessage, exception.getMessage());
     }
 }
