82a2e1a706f8cc81224db64832d3de240eddd60a
KSL-Pimentel <bruno.pimentel@sptech.school>
2024-05-29 11:34:37 -0300

    adicionando teste

---
+++ src/test/java/sptech/school/exemploactions/service/CalculadoraServiceTest.java
@@ -0,0 +1,22 @@
+package sptech.school.exemploactions.service;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalculadoraServiceTest {
+
+    @Test
+    @DisplayName("Quando adicionado com 10 e 2, então deve retornar 5")
+    public void testDividir() {
+        CalculadoraService calculadoraService = new CalculadoraService();
+        double a = 10;
+        double b = 2;
+        double esperado = 5;
+
+        double resultado = calculadoraService.dividir(a, b);
+
+        assertEquals(esperado, resultado);
+    }
+}
